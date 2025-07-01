f16b95e6038d65b0b6e17f504dfbed70a6a8e1e7
Philip Huang <22phuang@gmail.com>
2025-07-01 17:34:48 -0400

    begin parse_metadata

--- scripts/PH_parse_md_A.py
+++ scripts/PH_parse_md_A.py
@@ -10,9 +10,11 @@ import os
 import numpy as np
 from utils import calculate_HRR, calculate_MFR, colorize
 
-INPUT_DIR = Path(r"../data/raw/md_A")
+INPUT_DIR = Path(r"../data/raw/test")
 OUTPUT_DIR_CSV = Path(r"../data/auto-processed/md_A")
+METADATA_DIR = Path(r"../metadata/md_A")
 LOG_FILE = INPUT_DIR / "md_A_log.json"
+
 
 #region parse_dir
 # Find/load the Markdown files
@@ -84,7 +86,9 @@ def parse_file(file_path):
         # for each test, separate data from metadata
         test_data_df, metadata = get_data(tests[test])
         # generate test data csv
-        logfile = parse_data(test_data_df,test,file_path.name)
+        logfile,test_name = parse_data(test_data_df,test,file_path.name)
+        # parse through and generate metadata json file
+        parse_metadata(metadata,test_name)
         #update md_A_log.json
         with open(LOG_FILE, "w", encoding="utf-8") as f:
 	        f.write(json.dumps(logfile, indent=4))
@@ -127,6 +131,7 @@ def get_data(data):
     
     dataStart = -1
     dataEnd = -1
+    massWStart = -1
     index = 0
     #has_page = False
     for line in data:
@@ -146,6 +151,10 @@ def get_data(data):
                         dataStart = index
                     #has_page = True
                     break                    
+        # if "mass weighted", end of starting metadata chunk
+        if massWStart == -1 and ("MASS WEIGHTED" in line):
+            massWStart = index
+
         # mark ending of test data
         #if ("---" == line or index == len(data)-1) and has_page:
         if (("PARAMETER SHEET" in line) or index == len(data)-1):
@@ -155,8 +164,9 @@ def get_data(data):
         index += 1
 
     test_data = data[dataStart:dataEnd]
-    print(f"{dataStart} to {dataEnd}")
-    metadata = data[:dataStart] + data[dataEnd:]
+    #print(f"{dataStart} to {dataEnd}")
+    metadata = data[:massWStart] + data[dataEnd:]
+    print(f"{massWStart} to {dataEnd}")
     
     # convert test_data to df
     pd_format_test_data = StringIO("\n".join(test_data))
@@ -237,8 +247,8 @@ def parse_data(data_df,test,file_name):
     #generate test data csv name
     test_name = test.casefold()
     test_name = test_name.replace(" ", "")
-    test_name = test_name + "_" + file_name[0:8:1]
-    test_name = f"{test_name}.csv"
+    test_filename = test_name + "_" + file_name[0:8:1]
+    test_name = f"{test_filename}.csv"
 
     #generating contents for md_A_log
     with open(LOG_FILE, "r", encoding="utf-8") as w:  
@@ -267,10 +277,110 @@ def parse_data(data_df,test,file_name):
     data_df.to_csv(output_path, index=False)
     print(colorize(f"Generated {output_path}", "blue"))
 
-    return logfile
+    return logfile, test_filename
 
 
+####### metadata clean and output functions #######
+#region parse_metadata
+# clean and output metadata as json
+def parse_metadata(input,test_name):
+    test_name = "meta_" + test_name + ".json"
+    meta_path = METADATA_DIR / test_name
+    metadata_json = {}
+    metadata = []
 
+    # checking for existing test metadata file 
+    if meta_path.exists():
+        with open(meta_path, "r", encoding="utf-8") as w:  
+            metadata_json = json.load(w)
+
+    for line in input:
+        # finds all space blocks separating potential metadata values
+        # assumes metadata blocks are separated by at least 3 whitespaces
+        match_whitespace = re.search("\\s{3,}", line)
+        match_semicolon = re.search(";", line)
+        while match_whitespace is not None or match_semicolon is not None:
+
+            # checks which match is closer to the beginning (grabs only 1 metadata block)
+            if match_whitespace is not None:
+                match = match_whitespace
+            else:
+                match = match_semicolon
+            if match_whitespace is not None and match_semicolon is not None:
+                if match_semicolon.start() < match_whitespace.start():
+                    match = match_semicolon
+                else: 
+                    match = match_semicolon
+
+            metadata.append(line[:match.start()])
+            # trims line up to end of last match
+            line = line[match.end():]
+            match_whitespace = re.search("\\s{3,}", line)
+            match_semicolon = re.search(";", line)
+        if line.strip() != "":
+            metadata.append(line)
+
+    # metadata = list of metadata blocks as str
+    #print(metadata)
+
+    
+    ############ finding metadata fields ############
+    metadata_json["notes"] = []
+    for item in metadata:
+        if "HOR" in item:
+            metadata_json["orientation"] = "HORIZONTAL"
+        elif "VERT" in item:
+            metadata_json["orientation"] = "VERTICAL"
+        elif "IRRADIANCE" in item:
+            metadata_json["heat_flux_kW/m2"] = get_number(item,"int")
+        elif "CALIBRATION" in item:
+            metadata_json["c_factor"] = get_number(item[3:],"flt")
+        elif "INITIAL WEIGHT" in item:
+            metadata_json["initial_mass_g"] = get_number(item[3:],"flt")
+        elif "SURFACE AREA" in item:
+            metadata_json["surface_area_m2"] = get_number(item[3:],"flt")
+        elif "SOOT AVERAGE" in item:
+            metadata_json["soot_average"] = get_field(item)
+        else:
+            metadata_json["notes"].append(item) 
+            
+            
+
+    #update respective test metadata file
+    with open(meta_path, "w", encoding="utf-8") as f:
+        f.write(json.dumps(metadata_json, indent=4))
+
+    
+
+    # metadata table checker
+    # if line[0] == | then until not | at line[0] add to a table
+
+
+    # # Determine output path
+    # Path(OUTPUT_DIR_JSON / str(test_year)).mkdir(parents=True, exist_ok=True)
+
+    # data_output_path = Path(OUTPUT_DIR_JSON) / str(test_year) /f"{Path(file_path).stem}.csv"
+    # metadata_output_path = Path(OUTPUT_DIR_JSON) / str(test_year) / f"{Path(file_path).stem}.json"
+
+#get number(int, float,)
+def get_number(item, num_type):
+    match num_type:
+        case "int":
+            match = re.search(r'\d+', item)
+            if match:
+                number = int(match.group())
+        case "flt":
+            match = re.search(r'\d*\.\d+', item)
+            number = float(match.group())
+
+    return number
+
+#get content after the "="            
+def get_field(item):
+    index = item.find('=')
+    field = item[index+1:]
+    field = field.strip()
+    return field       
 
 #region main
 if __name__ == "__main__":
